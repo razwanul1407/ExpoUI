f845553a8d174b2f9eac2e69dedbc699f1939b0a
Md. Razwanul Hasan <razwanulhasan@gmail.com>
2023-09-12 12:22:08 +0600

    new

---
+++ src/navigation/Src.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Src = () => {
+  return (
+    <View>
+      <Text>Src</Text>
+    </View>
+  );
+};
+
+export default Src;
